3a549778dd0c05f56d0a1d974c59a2419db69c17
Plimon Howard <plimon.howard@gmail.com>
2020-10-15 17:48:41 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,10 @@
 class Meal
+  attr_accessor :waiter, :customer, :total, :tip
 
+  def initialize(waiter,customer,total,tip)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+  end
 end
