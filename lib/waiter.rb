066c025c910599d55195c7c415e45414074428dd
Plimon Howard <plimon.howard@gmail.com>
2020-10-15 17:59:02 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -11,4 +11,16 @@ class Waiter
   def self.all
     @@all
   end
+
+  def new_meal(customer,total,tip)
+    nM = Meal.new(self,customer,total,tip)
+  end
+
+  def meals
+
+  end
+
+  def best_tipper
+
+  end
 end
